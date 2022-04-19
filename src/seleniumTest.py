969cfa211403922dcad6f85f5b79c649d9559264
root <root@Will-T-PC.localdomain>
2022-04-19 02:41:00 -0500

    Cleaned up some of the data and got the english defitions for each word. Some of the data is bad, i.e. missing defitions and needs to be manually fixed. I will add an option at the end to manually define any invalid words. After that, I will actually add the words to an anki deck! Then I just haveto make the code dynamic so that it can make decks for all 5 of the different vocab sets.

--- src/seleniumTest.py
+++ src/seleniumTest.py
@@ -2,8 +2,6 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 import time
-naverUserName = "destinyisgood4221"
-naverPassWord = "Willdog4221"
 
 ser = Service("C:/Users/21wil/Documents/chromedriver.exe")
 op = webdriver.ChromeOptions()
@@ -13,15 +11,39 @@ driver = webdriver.Chrome(service=ser, options=op)
 # driver.get('https://learn.dict.naver.com/wordbook/koendict/#/my/cards?')
 driver.get('https://learn.dict.naver.com/wordbook/koendict/#/my/cards?wbId=c67e217668794f2e93cc2653144169c4')
 
-time.sleep(1)
+time.sleep(.3)
 
-for word in range(1, 21):
-    xpath = '//*[@id="section_word_card"]/div[' + str(word) + ']/div[1]/div[1]/div[1]/div/a'
-    koreanWord = driver.find_element(By.XPATH, xpath)
-    print(koreanWord.text)
-# koreanWord = driver.find_element(By.XPATH, '//*[@id="section_word_card"]/div[1]/div[1]/div[1]/div[1]/div/a')
-# //*[@id="section_word_card"]/div[1]/div[1]/div[1]/div[1]/div/a
-# //*[@id="section_word_card"]/div[2]/div[1]/div[1]/div[1]/div/a
-# //*[@id="section_word_card"]/div[20]/div[1]/div[1]/div[1]/div/a
+invalidWords = []
 
-# print(koreanWord.text)
+xpathPageTotal = '//*[@id="page_area"]/div/div/span[3]'
+pages = int(driver.find_element(By.XPATH, xpathPageTotal).text)
+
+for page in range(1, pages+1):
+    xpathNextPage = '//*[@id="page_area"]/div/button[2]'
+    nextPage = driver.find_element(By.XPATH, xpathNextPage)
+    xpathWords = '//*[@id="section_word_card"]'
+    words = len(driver.find_element(By.XPATH, xpathWords).find_elements(By.CLASS_NAME, 'card_word '))
+    for word in range(1, words+1):
+        xpathKorean = '//*[@id="section_word_card"]/div[' + str(word) + ']/div[1]/div[1]/div[1]/div/a'
+        xpathDef = '//*[@id="section_word_card"]/div[' + str(word) + ']/div[1]/div[2]/div/ul'
+        koreanWord = driver.find_element(By.XPATH, xpathKorean)
+        definitions = driver.find_element(By.XPATH, xpathDef)
+        print(koreanWord.text.split(" ")[0])
+        defCnt = 0
+        for d in definitions.text.split("\n"):
+            if d.count(' ') >= 2:
+                defCnt += 1
+                # print(d[0:d.find('.')] + ' ' + d[d.find(' ', 3):].strip())
+            else:
+                print('ERROR: ' + d[0:d.find('.')] + d[d.find('.')+1:])
+        if defCnt < 1:
+            invalidWords.append(koreanWord.text.split(" ")[0])
+    print('\n---------------- Page' + str(page) + ' ----------------\n')
+    nextPage.click()
+    time.sleep(.3)
+
+print('Words with no definitions: ')
+print(invalidWords)
+
+driver.close()
+
